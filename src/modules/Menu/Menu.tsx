1314050d9689d62027fa099db376bbc09303c28f
Tamila Kochereshchenko <tamila272005@icloud.com>
2026-06-24 15:00:02 +0300

    add nav

--- src/modules/Menu/Menu.tsx
+++ src/modules/Menu/Menu.tsx
@@ -1,9 +1,41 @@
-export const Menu = () => {
+import { Link, NavLink } from 'react-router-dom';
+
+interface MenuItem {
+  title: string;
+  path: string;
+}
+
+const menuItems: MenuItem[] = [
+  { title: 'HOME', path: '/' },
+  { title: 'PHONES', path: '/phones' },
+  { title: 'TABLETS', path: '/tablets' },
+  { title: 'ACCESSORIES', path: '/accessories' },
+];
+
+export const Menu: React.FC = () => {
   return (
-    <div>
-      <a href="/" aria-label="Головна сторінка">
+    <header className="header">
+      <Link to="/" className="header__logo" aria-label="Головна сторінка">
         <img src="/img/Logo.png" alt="NICE GADGETS" />
-      </a>
-    </div>
+      </Link>
+
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          {menuItems.map(item => (
+            <li key={item.path}>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? 'header__nav_link header__nav_link--active'
+                    : 'header__nav_link'
+                }
+              >
+                {item.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 };
